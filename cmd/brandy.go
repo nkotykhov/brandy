a1b6e80f4be7e773d6bc5e286f953ec133f0dc78
Nikolay Kotykhov <nikolay.kotykhov@gmail.com>
2020-12-11 02:47:00 -0700

    building up command functioning
    brandy now takes two arguments: rpm and basepath.  It reads RPM
    and if finds .spec in payload writes it to SPECS under basepath

--- cmd/brandy.go
+++ cmd/brandy.go
@@ -6,58 +6,77 @@ import (
 	"github.com/rocky-linux/brandy/rpm"
 	"github.com/rocky-linux/brandy/rpm/rpmutil"
 	"os"
+	"path"
+	"path/filepath"
 )
 
-func playground(pkg, dir string) error {
-	f, err := os.Open(pkg)
-	if err != nil {
-		return err
+func srpm2git(pkg *rpmutil.Package, basedir string) error{
+	specDir := path.Join(basedir, "SPECS")
+	if _, err := os.Stat(specDir); os.IsNotExist(err) {
+		err := os.Mkdir(specDir, 0760);
+		if err != nil {
+			return err
+		}
 	}
-	defer f.Close()
-	p, err := rpmutil.ReadPackage(f)
-
-	if err != nil {
-		return err
-	}
-	_, name, err := p.Header.GetTag(rpm.TagName)
-	if err != nil {
-		return err
-	}
-	_, version, err := p.Header.GetTag(rpm.TagVersion)
-	fmt.Printf("%s-%s\n", name, version)
-	pr, err := p.Payload();
-
+	payload, err := pkg.Payload();
 	if err != nil {
 		return err
 	}
 	for {
-		hdr,f, err := pr.Next()
+		h, f, err := payload.Next()
 		if err != nil {
-			if err == io.EOF {
-				return nil
-			}
 			return err
 		}
-		fmt.Printf("fileName: %s\n", hdr.Name)
-		fheader := make([]byte, 8);
-		if _, err := f.Read(fheader); err != nil {
-			fmt.Printf("unable to read file: %s\n", err.Error())
-			continue
+		if filepath.Ext(h.Name) == ".spec" {
+			err := func () error {
+				dest := path.Join(specDir, h.Name)
+				out, err := os.Create(dest)
+				if err != nil {
+					return err
+				}
+				defer out.Close()
+				_, err = io.Copy(out, f)
+				if err != nil && err != io.EOF {
+					panic(err)
+					return err
+				}
+				return nil
+			}()
+			if err != nil {
+				return err
+			}
+			return nil
 		}
-		fmt.Printf("first 8 bytes are: %s\n", fheader)
 	}
-
 	return nil
-
 }
 
+
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "usage %s [file]\n", os.Args[0]);
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "usage %s [file] [path]\n", os.Args[0]);
 		os.Exit(1)
 	}
 
-	err := playground(os.Args[1], "")
+	if _, err := os.Stat(os.Args[2]); os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "base directory doesn't exits\n")
+		os.Exit(1)
+	}
+
+	pkgf, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to open package file: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	defer pkgf.Close()
+	pkg, err := rpmutil.ReadPackage(pkgf)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed during package parsing: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	err = srpm2git(pkg, os.Args[2])
 	if err != nil {
 		panic(err)
 	}
